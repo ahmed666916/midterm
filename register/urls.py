fedc6a41822b07b1662152600f40be4bc4c46ade
ahmed666916 <ahmed666916@gmail.com>
2023-06-02 01:46:20 +0500

    added home.html page

--- register/urls.py
+++ register/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
     path('register/', views.sign_up, name='register'),
--- register/views.py
+++ register/views.py
@@ -50,4 +50,7 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     messages.success(request,f'You have been logged out.')
-    return redirect('login')   
+    return redirect('login')
+
+def home(request):
+    return render(request, 'users/home.html')  
